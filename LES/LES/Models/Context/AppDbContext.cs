3a29251a97c9abf26291a6ad8e46db4cbe0534fb
RAI SILVA <rai.benjamindasilva@gmail.com>
2021-02-14 17:17:34 -0300

    contexto configurado, feita a primeira migração

--- LES/LES/Models/Context/AppDbContext.cs
+++ LES/LES/Models/Context/AppDbContext.cs
@@ -10,8 +10,14 @@ namespace LES.Models
     public class AppDbContext : DbContext
     {
 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+        
+        }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
