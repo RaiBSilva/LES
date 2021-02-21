c954c07de4967a535dd032bac8f2c17aee0b4166
RAI SILVA <rai.benjamindasilva@gmail.com>
2021-02-21 12:45:14 -0300

    iniciado trabalho no javascript que adicionará endereços de entrega de acordo com o pedido do cliente

---
+++ LES/LES/wwwroot/js/GerenciadorEnderecos.js
@@ -0,0 +1,14 @@
+﻿function adicionarEndereco(tipo, div) {
+
+    var newDivEndereco = document.createElement("div");
+    newDivEndereco.className = "endereco";
+    div.insertBefore(newDivEndereco, div.lastChild)
+
+    var newHeader = document.createElement("h2");
+    newHeader.innerHTML = "Endereço" + div.childElementCount;
+    newDivEndereco.appendChild(newHeader);
+
+    var newDivTipoEndereco = document.createElement("div");
+    newDivTipoEndereco = "form-group";
+
+}
