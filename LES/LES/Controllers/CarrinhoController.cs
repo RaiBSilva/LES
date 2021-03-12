c2d73dca942db0ef4717c7d941404ad36d11efb0
RAI SILVA <rai.benjamindasilva@gmail.com>
2021-03-12 01:14:17 -0300

    carrinho de compras em dropdown implementado

--- LES/LES/Controllers/CarrinhoController.cs
+++ LES/LES/Controllers/CarrinhoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LES.Models.ViewModel.Carrinho;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,32 @@ namespace LES.Views.Carrinho
 {
     public class CarrinhoController : Controller
     {
+
+        CarrinhoModel Model = new CarrinhoModel();
+
+        public CarrinhoController()
+        {
+            Model.Livros = new List<CarrinhoLivroModel>();
+
+            CarrinhoLivroModel livro = new CarrinhoLivroModel();
+
+            livro.Titulo = "Winds of Winter";
+            livro.Autor = "George R. R. Maritn";
+            livro.Preco = 100;
+            livro.Quantia = 3;
+
+            Model.Livros.Add(livro);
+
+        }
+
         public IActionResult Carrinho()
         {
             return View();
+        }
+
+        public IActionResult _CarrinhoPartial() 
+        {
+            return PartialView("_CarrinhoPartial", Model);
         }
     }
 }
--- LES/LES/wwwroot/js/site.js
+++ LES/LES/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function carregarCarrinho(endereco) {
+
+    var divDropdown = document.getElementById("dropdown");
+
+    $(divDropdown).load(endereco);
+}
+
+$(".carrinho-update").on("click", function (e) {
+    carregarCarrinho(carrinhoUrl);
+});
+
+carregarCarrinho(carrinhoUrl);
