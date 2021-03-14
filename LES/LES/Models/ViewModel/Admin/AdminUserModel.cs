d38e978f7582e4a609abbd18925f69acc6bbda42
RAI SILVA <rai.benjamindasilva@gmail.com>
2021-03-14 00:49:36 -0300

    front pronto

---
+++ LES/LES/Models/ViewModel/Admin/AdminUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LES.Models.ViewModel.Admin
+{
+    public class AdminUserModel
+    {
+
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ LES/LES/Views/Admin/Pedidos.cshtml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LES.Views.Administrador
+{
+    public class ListaTransacoesModel : PageModel
+    {
+
+    }
+}
