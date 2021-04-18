b05971c287f49ffde05a25e4424dec599c3cdabc
lCamiloR <luccashu3@yahoo.com.br>
2021-04-18 20:17:27 -0300

    strategy para data de nascimento

---
+++ LES/LES/Negócio/Strategy/ClienteStrategy/ValidaDataNascimentoStrategy.cs
@@ -0,0 +1,24 @@
+﻿using LES.Models.Entity;
+using LES.Negocio.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LES.Negócio.Strategy.ClienteStrategy
+{
+    public class ValidaDataNascimentoStrategy : IStrategy<EntidadeDominio>
+    {
+        public string Validar(EntidadeDominio e)
+        {
+            Cliente cliente = (Cliente)e;
+
+            DateTime dataNascimento = cliente.DtNascimento;
+            DateTime dataAtual = DateTime.Now;
+
+            if (dataNascimento >= dataAtual) return "Data de nascimento inválida.";
+
+            return "";
+        }
+    }
+}
