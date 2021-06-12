01dd7b5b5366a02d65068949cac7cee6a3fc55fc
lCamiloR <luccas@bpatechnologies.com>
2021-06-12 11:46:50 -0300

    alteração nos testes

--- LES/LES/Controllers/AdminController.cs
+++ LES/LES/Controllers/AdminController.cs
@@ -1102,7 +1102,7 @@ namespace LES.Controllers
                 foreach(var (Month, Year) in listaMeses)
                 {
                     int contagem = livro.LivroPedidos
-                        .Where(l => (dateTimeFormat.GetMonthName(l.DtCadastro.Month) == Month && l.DtCadastro.Year == Year)&&(l.Pedido.Status != StatusPedidos.NaoFinalizado))
+                        .Where(l => (dateTimeFormat.GetMonthName(l.DtCadastro.Month) == Month && l.DtCadastro.Year == Year)&&(l.Pedido.Status != StatusPedidos.Aprovado || l.Pedido.Status != StatusPedidos.Entregue || l.Pedido.Status != StatusPedidos.EmTransito))
                         .Count();
                     data.Add(contagem);
                 }
